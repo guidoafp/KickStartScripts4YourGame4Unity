fc4874429e3c2adcea410e9168de2c5be1101f5d
Guido Pereira <41960014+guidoafp@users.noreply.github.com>
2020-11-22 20:30:58 -0300

    Fifth Script - object spin
    This is the smallest script ever, the game "Fall guys" obstacles inspired me for this script. Have Fun!

---
+++ SimpleSpin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// This Script make the object spin around! 
+/// Version of Unity used on this script 2019.3.7f1 LTS(Long Term Support)
+/// This Script was used in 3D mode and in a top view. 
+/// The object you assigned this script must have a Rigidbody with a high mass number to and other collising objects as well.
+/// The intent of this script is to help beginners and those that have limited code skills to make games!
+/// Have Fun!
+
+public class SimpleSpin : MonoBehaviour
+{
+	public float speed = 1000f;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(Vector3.up, speed * Time.deltaTime * 5);
+	}
+}
